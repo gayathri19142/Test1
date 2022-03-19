edea92a50aa18036e00a1e91f885935ee1640399
gayathri19142 <101949038+gayathri19142@users.noreply.github.com>
2022-03-19 15:19:34 -0500

    Create Condition.java

---
+++ Condition.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Condition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean repeatCondition = true;
+        while (repeatCondition) {
+            System.out.println("Playing current song");
+            System.out.println("Do you want to repeat");
+            String userInput = input.next();
+            if (userInput.equals("yes")) {
+
+            } else {
+                System.out.println("Play Next song");
+                repeatCondition = false;
+            }
+        }
+
+    }
+}
